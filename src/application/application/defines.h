f7ff5dd837f01698fbfb1be8390790cf9fa0f848
Thomas Andersen <thoander@stud.ntnu.no>
2021-10-04 18:50:52 +0200

    add comments

--- src/application/application/defines.h
+++ src/application/application/defines.h
@@ -13,6 +13,10 @@
 
 #define BUILTIN_LED PORTB5 // (Arduino Uno pin 13)
 
+/*
+	Be aware that the application will not function as intended if the pins are not defined on the same port as they are now
+*/
+
 /******** Encoder left ********/
 #define ENCA1 PORTD2 // (INT0) (Arduino Uno pin 2)
 #define ENCB1 PORTD4 // (Arduino Uno pin 4)
@@ -25,8 +29,8 @@
 /*************************/
 
 /******** PWM (8-bit) *********/
-#define PWM1 PD6 //OC0A // (PD6)
-#define PWM2 PD5 //OC0B // (PD5)
+#define PWM1 PD6 //OC0A // (Arduino Uno pin 6)
+#define PWM2 PD5 //OC0B // (Arduino Uno pin 5)
 
 
 
