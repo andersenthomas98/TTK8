c5c19d3ae10516d6f0f40f1d5348fa6e7dd1ae50
Thomas Andersen <thoander@stud.ntnu.no>
2021-10-22 13:50:13 +0200

    setters

--- src/application/application/drivers/speed_controller/speed_controller.c
+++ src/application/application/drivers/speed_controller/speed_controller.c
@@ -4,3 +4,34 @@
  * Created: 22.10.2021 13:09:25
  *  Author: thoander
  */ 
+
+#include "speed_controller.h"
+#include "speed_estimator.h"
+#include "timer.h"
+
+float reference_speed = 0.0;
+float Kp = 0.0;
+float Ki = 0.0;
+float Kd = 0.0;
+
+void set_reference_speed(float ref) {
+	ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
+		reference_speed = ref;
+	}
+}
+
+void set_PID_parameters(float P, float I, float D) {
+	ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
+		Kp = P;
+		Ki = I;
+		Kd = D;
+	}
+}
+
+void start_PID(){
+	
+}
+
+void stop_PID() {
+	
+}
